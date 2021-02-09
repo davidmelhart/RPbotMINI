1de3de63771a3011f6b4be859dbb3efde8fa4444
David <david.melhart@gmail.com>
2021-02-09 01:49:35 +0100

    Multiple characters per users. Error handling

--- README.md
+++ README.md
@@ -1,37 +1,26 @@
 # RPbotMINI
-A minimalistic Discord bot that let's you speak in character without any prefixes.
-You can register your characters per **channel** like this:
-`!register @username "character name" https://portrait.png #hexcolor`
+A minimalistic Discord bot that let's you speak in character without any prefixes. The bot catches your messages, deletes them, and post their content as your active "speak as" character in the given channel.
 
-If you made a mistake, or want to update your character, you can just run the command again at it will overwrite what you have. You can do this separately for every channel!
+The characters are saved in a JSON database. The database saves characters for each guild>channel>user, so you have to have characters with unique names on the same **channel** but not necessarily on the same guild.
 
-Characters are saved in the `characters.json` file which is created first time you register a character so you won't have to set them up every time but keep a copy just in case.
+You can register a character with the `!register "character name" https://portrait.png #hexcolor` command. **Name** and **portrait** are mandatory fields, **hexcolor** is optional. If you don't put in a colour, the bot will assign one randomly. 
 
-Your messages will automatically be deleted and the bot will push the same message **as** your character. To talk out of character you can use the `!ooc` or just the `!` prefix (for now).
+The characters are indexed by their name. If you made a mistake or want to update the **portrait or colour** of your character, you can just run the command again and it will overwrite the existing one.
 
-If you want to delete the character you can do so like this (this will only clear your character in the current channel) this will also delete the character from storage:
-`!unregister @username`
+If you want to delete the character (to for example give it another name) you can do so with the `!unregister "character name"` command.
 
-If you don't want to delete the character just want to turn off the in-character speech, you can deactivate your character like this:
-`!deactivate @username`
+If you want to clear all your characters on the current channel you can use the `!clear` command. If you want to delete all your characters from the whole **guild**, you can use the `!delete` command. 
 
-To activate it again just type:
-`!activate @username`
+**PLEASE BE AWARE THAT DELETING CHARACTERS IS FINAL! RUNNING `!unregister`, `!clear`, AND `!delete` ALSO REMOVES THE CHARACTERS FROM THE DATABASE!**
 
+You can register multiple characters on one channel (useful for recurring NPCs). You can switch between characters with the `!as "character name"` command.
 
-Finally, if you want to speak as an NPC you can use the following syntax:
-`!as NPC_name message`
-(The `_` will be converted to spaces in the NPC's name.)
+You can get a list of your characters on the channel with `!list`. You can also request a list of another person's characters with `!list @user`.
 
-Handouts work as well, with normal file uploads! The comment and the file will be separated. The file itself won't have a portrait attached but will still have the name and colour of the character.
+You can talk **out of character** with the `!ooc` or just the `!` prefix (for now). Alternatively, you can turn off the in-character speech with `!deactivate`. You can turn it back on with the `!activate` command.
 
-The bot cleans up after itself, if a command is successful, you should see it disappear.
+Finally you can speak as a random NPC with this syntax: `!npc NPC_name message` (The `_` characters will be converted to spaces in the NPC's name.)
 
-## TODO:
-More GM tools: support for recurring NPCs with portraits
-- refactor the code so the character storage is organised based on users
-- let users register multiple characters per channel
-- change `!as` to switch between characters
-- move `!as` funcionality to `!npc`
+You can use uploads as well to create handouts. Comments and the file will be sent as separate messages. The file itself won't have a portrait attached but will still have the name and colour of the character.
 
-- Handle errors with partially filled out character sheets
+The bot cleans up after itself, if a command is successful, you should see it disappear. There is some minimal error handling through DMs. DM messages are kept for **one minute** after which they are automatically deleted. If for some reason the messages stick, you can use the `!purge` command in the bot DMs to clear the last 100 messages sent by the bot.
--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import os
 import json
 import discord
+import random
+import typing
+import colorsys
 from discord.ext import commands
 from dotenv import load_dotenv
 from discord.ext.commands import CommandNotFound
@@ -33,20 +36,113 @@ async def on_ready():
 
 
 @bot.command(name='register')
-async def register(ctx, user_name, character_name, portrait, color):
-    if color.startswith("#"):
+async def register(ctx, character_name, portrait: typing.Optional[str], color: typing.Optional[str]):
+    if not color:
+        h, s, l = random.random(), random.uniform(0.5, 0.7), random.uniform(0.5, 0.7)
+        r, g, b = [int(256 * i) for i in colorsys.hls_to_rgb(h, l, s)]
+        color = "{0:02x}{1:02x}{2:02x}".format(r, g, b)
+    elif color.startswith("#"):
         color = color[1:]
+
+    if not portrait:
+        portrait = 'https://cdn.discordapp.com/embed/avatars/1.png'
+    elif not portrait.startswith('http'):
+        portrait = 'https://cdn.discordapp.com/embed/avatars/1.png'
+
+    guild = str(ctx.guild.id)
+    channel = str(ctx.message.channel.id)
+    user = '{}'.format(ctx.message.author.id)
+
     character = {
         "name": character_name,
         "portrait": portrait,
         "color": color,
-        "active": True
     }
+
+    if channel not in characters[guild]:
+        characters[guild][channel] = {}
+
+    if user not in characters[guild][channel]:
+        characters[guild][channel][user] = {}
+        characters[guild][channel][user]['characters'] = {}
+
+    characters[guild][channel][user]['active'] = True
+    characters[guild][channel][user]["characters"][character_name] = character
+    characters[guild][channel][user]['as'] = character_name
+    await ctx.message.delete()
+
+    with open('characters.json', 'w') as fp:
+        json.dump(characters, fp)
+
+
+@bot.command(name='list')
+async def character_list(ctx, user_name: typing.Optional[discord.Member]):
+    guild = str(ctx.guild.id)
+    channel = str(ctx.message.channel.id)
+    if not user_name:
+        user = '{}'.format(ctx.message.author.id)
+    else:
+        user = '{}'.format(user_name.id)
+
     try:
-        characters[str(ctx.guild.id)][str(ctx.message.channel.id)][user_name.translate({ord(c): '' for c in clean_id})] = character
+        c_list = characters[guild][channel][user]['characters']
+        if len(c_list) > 0:
+            if not user_name:
+                await ctx.message.author.send("Your characters registered on {}:"
+                                              .format(ctx.message.channel.name), delete_after=60)
+            else:
+                await ctx.message.author.send("Characters of {} registered on {}:"
+                                              .format(user_name.display_name, ctx.message.channel.name), delete_after=60)
+            for c in c_list:
+                character = c_list[c]
+                embed = discord.Embed(color=int("0x{}".format(character['color']), 16))
+                embed.set_thumbnail(url=character['portrait'])
+                embed.set_author(name=character['name'])  # icon_url=character['portrait']
+
+                await ctx.message.author.send(embed=embed, delete_after=60)
+        else:
+            if not user_name:
+                await ctx.message.author.send("You have no characters registered on {}."
+                                              .format(ctx.message.channel.name), delete_after=60)
+            else:
+                await ctx.message.author.send("{} has no characters registered on {}."
+                                              .format(user_name.display_name, ctx.message.channel.name), delete_after=60)
     except KeyError:
-        characters[str(ctx.guild.id)][str(ctx.message.channel.id)] = {}
-        characters[str(ctx.guild.id)][str(ctx.message.channel.id)][user_name.translate({ord(c): '' for c in clean_id})] = character
+        if not user_name:
+            await ctx.message.author.send("You have no characters registered on {}."
+                                          .format(ctx.message.channel.name), delete_after=60)
+        else:
+            await ctx.message.author.send("{} has no characters registered on {}."
+                                          .format(user_name.display_name, ctx.message.channel.name), delete_after=60)
+    await ctx.message.delete()
+
+
+@bot.command(name='clear')
+async def clear_channel(ctx):
+    guild = str(ctx.guild.id)
+    channel = str(ctx.message.channel.id)
+    user = '{}'.format(ctx.message.author.id)
+    try:
+        del characters[guild][channel][user]
+    except KeyError:
+        await ctx.message.author.send("You have no characters registered on {}."
+                                      .format(ctx.message.channel.name), delete_after=60)
+    await ctx.message.delete()
+
+    with open('characters.json', 'w') as fp:
+        json.dump(characters, fp)
+
+
+@bot.command(name='delete')
+async def delete_user(ctx):
+    guild = str(ctx.guild.id)
+    user = '{}'.format(ctx.message.author.id)
+
+    for channel in characters[guild].keys():
+        try:
+            del characters[guild][channel][user]
+        except KeyError:
+            pass
     await ctx.message.delete()
 
     with open('characters.json', 'w') as fp:
@@ -54,11 +150,48 @@ async def register(ctx, user_name, character_name, portrait, color):
 
 
 @bot.command(name='unregister')
-async def unregister(ctx, user_name):
+async def unregister(ctx, character_name):
+    guild = str(ctx.guild.id)
+    channel = str(ctx.message.channel.id)
+    user = '{}'.format(ctx.message.author.id)
+
     try:
-        del characters[str(ctx.guild.id)][str(ctx.message.channel.id)][user_name.translate({ord(c): '' for c in clean_id})]
+        del characters[guild][channel][user]["characters"][character_name]
+        character_list = characters[guild][channel][user]["characters"]
+        if characters[guild][channel][user]["as"] == character_name and len(character_list) > 0:
+            random_character = character_list[random.choice(list(character_list.keys()))]
+            characters[guild][channel][user]["as"] = random_character['name']
+
+            embed = discord.Embed(description="{} unregistered, now speaking as {}.\n"
+                                              "To get a list of available characters in the channel call:\n"
+                                              "`!list @username`"
+                                  .format(character_name, random_character['name']),
+                                  color=int("0x{}".format(random_character['color']), 16))
+            embed.set_thumbnail(url=random_character['portrait'])
+            embed.set_author(name=random_character['name'])  # icon_url=character['portrait']
+
+            await ctx.message.author.send(embed=embed, delete_after=60)
     except KeyError:
-        pass
+        await ctx.message.author.send("This character is not registered on {}.\n"
+                                      "To get a list of available characters in the channel call: `!list`"
+                                      .format(ctx.message.channel.name), delete_after=60)
+    await ctx.message.delete()
+
+    with open('characters.json', 'w') as fp:
+        json.dump(characters, fp)
+
+
+@bot.command(name='as')
+async def speak_as(ctx, character_name):
+    guild = str(ctx.guild.id)
+    channel = str(ctx.message.channel.id)
+    user = '{}'.format(ctx.message.author.id)
+    try:
+        characters[guild][channel][user]['as'] = characters[guild][channel][user]['characters'][character_name]['name']
+    except KeyError:
+        await ctx.message.author.send("This character is not registered on {}.\n"
+                                      "To get a list of available characters in the channel call: `!list`"
+                                      .format(ctx.message.channel.name), delete_after=60)
     await ctx.message.delete()
 
     with open('characters.json', 'w') as fp:
@@ -66,20 +199,28 @@ async def unregister(ctx, user_name):
 
 
 @bot.command(name='activate')
-async def activate(ctx, user_name):
+async def activate(ctx):
+    guild = str(ctx.guild.id)
+    channel = str(ctx.message.channel.id)
+    user = '{}'.format(ctx.message.author.id)
     try:
-        characters[str(ctx.guild.id)][str(ctx.message.channel.id)][user_name.translate({ord(c): '' for c in clean_id})]['active'] = True
+        characters[guild][channel][user]['active'] = True
     except KeyError:
-        pass
+        await ctx.message.author.send("This user has no characters registered on {}."
+                                      .format(ctx.message.channel.name), delete_after=60)
     await ctx.message.delete()
 
 
 @bot.command(name='deactivate')
-async def deactivate(ctx, user_name):
+async def deactivate(ctx):
+    guild = str(ctx.guild.id)
+    channel = str(ctx.message.channel.id)
+    user = '{}'.format(ctx.message.author.id)
     try:
-        characters[str(ctx.guild.id)][str(ctx.message.channel.id)][user_name.translate({ord(c): '' for c in clean_id})]['active'] = False
+        characters[guild][channel][user]['active'] = False
     except KeyError:
-        pass
+        await ctx.message.author.send("This user has no characters registered on {}."
+                                      .format(ctx.message.channel.name), delete_after=60)
     await ctx.message.delete()
 
 
@@ -99,13 +240,20 @@ async def on_command_error(ctx, error):
 async def on_message(message):
     if not message.author.bot:
         if not message.content.startswith("!"):
+            guild = str(message.guild.id)
+            channel = str(message.channel.id)
+            user = '{}'.format(message.author.id)
+
             try:
-                character = characters[str(message.guild.id)][str(message.channel.id)]['{}'.format(message.author.id)]
-                if character['active']:
+                user_data = characters[guild][channel][user]
+                speak_as = user_data['as']
+                character = user_data['characters'][speak_as]
+
+                if user_data['active']:
                     embed = discord.Embed(description=message.content, color=int("0x{}".format(character['color']), 16))
 
                     embed.set_thumbnail(url=character['portrait'])
-                    embed.set_author(name=character['name']) # icon_url=character['portrait']
+                    embed.set_author(name=character['name'])  # icon_url=character['portrait']
 
                     await message.delete()
                     if len(message.content) > 0:
@@ -120,7 +268,8 @@ async def on_message(message):
                     await bot.process_commands(message)
             except KeyError:
                 pass
-        if message.content.startswith("!as"):
+
+        if message.content.startswith("!npc"):
             msg = message.content.split(" ")
             npc = " ".join(msg[1].split('_'))
             embed = discord.Embed(description=" ".join(msg[2:]))
@@ -131,5 +280,13 @@ async def on_message(message):
             await message.delete()
             await message.channel.send(embed=embed)
             await bot.process_commands(message)
+
+
+@bot.command(name='purge')
+async def purge(ctx):
+    if ctx.guild is None:
+        async for message in ctx.message.channel.history(limit=100):
+            if message.author.id == bot.user.id:
+                await message.delete()
 
 bot.run(TOKEN)
